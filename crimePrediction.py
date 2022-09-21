355e1a6ce9fa113aa5729f25c56a93a625a67336
Dilip-Sapkota <88530698+Dilip-Sapkota@users.noreply.github.com>
2022-09-21 16:25:04 +0930

    changes made

--- crimePrediction.py
+++ crimePrediction.py
@@ -45,18 +45,105 @@ print(extractedDf.tail())
 inOut= {  
          "ALLEY" : "OUT",
          "APARTMENT" : "IN", 
+          "APT.LOCKE" : "IN",
+         "APT/CONDO" : "IN",
+         "ATM MACHIN" : "IN",
+         "AUTO PARTS" : "IN",
+         "BAKERY" : "IN",
+         "BANK/FINAN" : "IN",
+         "BAR" : "IN",
+         "BARBER/BEA" : "IN",
+         "BRIDGE-PIE" : "OUT",
+         "BUS. STOR" : "OUT",
+         "BUS. PARK" : "OUT",
+         "BUS/RAILRO" : "OUT"
+         "BUS/AUTO" : "OUT"
+         "CAB" : "OUT",
+         "CAR REPAI" : "OUT"
+         "CAR LOT-NE" : "OUT"
+         "CARRY OUT" : "OUT"
+         "CEMETERY" : "OUT",
+         "CHAIN FOOD" : "IN",
+         "CHURCH" : "IN",
+         "CLOTHING" : "IN",
+         "CLUB HOUSE" : "IN",
+         "COMMON BUS" : "OUT",
+         "CONSTRUCTI" : "OUT",
+         "CONVENIENC" : "OUT",
+         "CONVENTION" : "OUT",
+         "COURT HOUS" : "IN",
+         "DEPARTMENT" : "IN", 
+         "DRIVEWAY" : "OUT",
+         "DRUNG STORE" : "IN",
+         "DWELLING" : "OUT",
+         "FAST FOOD" : "IN",
+         "FINANCE/LO" : "IN",
+         "FIRE DEPAR" : "IN",
+         "GARAGE" : "OUT",
+         "GARAGE ON" : "OUT",
+         "GAS STATIO" : "OUT",
+         "GROCERY/CO" : "IN",
+         "HARDWARE/B" : "IN",
+         "HOSP/NURS." : "IN"
+         "HOSPITAL" : "IN"
+         "HOTEL/MOTE" : "IN",
+         "INNER HARB" : "OUT"
+         "JEWELRY ST" : "IN"
+         "LAUNDRY/CL" : "IN"
+         "LIBRARY" : "IN",
+         "LIGHT RAIL" : "OUT",
+         "LIQUOR STO" : "OUT",
+         "MARKET STA" : "IN",
+         "MTA LOT" : "IN",
+         "NIGHT DEPO" : "OUT",
+         "OFFICE BUI" : "IN",
+         "OTHER - IN" : "IN",
+         "OTHER - OU" : "OUT",
+         "OTHER/RESI" : "IN",
+         "PARK" : "OUT",
+         "PARKING LO" : "OUT",
+         "PIZZA/OTHE" : "IN",
+         "PLAYGROUND" : "OUT",
+         "POLICE DEP" : "IN",
+         "POOL/BOWLI" : "OUT",
+         "PORCH/DECK : "OUT",
+         "Private Sc" : "IN",
+         "Public Are" : "OUT",
+         "PUBLIC BUI" : "OUT",
+         "PUBLIC HOU" : "OUT",
+         "Public Sch" : "IN",
+         "RACE TRACK" : "OUT"
+         "RAILROAD C" : "OUT"
+         "RECREATION" : "IN",
+         "RELIGIOUS" : "OUT"
+         "RENTAL/VID" : "OUT"
+         "RESTAURANT" : "OUT"
+         "RETAIL/SMA" : "IN",
          "ROW/TOWNHO" : "IN",
-        #  "Street" : "STREET",
-        #  "STREET" : "STREET",
-        #  "Street" : "STREET",
-        #  "STREET" : "STREET",
-        #  "Street" : "STREET",
-        #  "STREET" : "STREET",
-        #  "Street" : "STREET",
-        #  "STREET" : "STREET",
-        #  "Street" : "STREET",
-        #  "STREET" : "STREET",
-        #  "Street" : "STREET",
+         "SALESMAN/C" : "IN",
+         "SCHOOL" : "IN",
+         "SCHOOL PLA" : "IN",
+         "SHED/GARAG" : "IN",
+         "SHOPPING M" : "IN",
+         "SINGLE HOU" : "OUT",
+         "SKYWALK" : "OUT",
+         "SPECIALITY" : "IN",
+         "STADIUM" : "OUT", 
+         "Street" : "OUT",
+         "STRUCTURE" : "OUT",
+         "SUBWAY" : "OUT",
+         "TAVERN/NIG" : "OUT"
+         "THEATRE" : "OUT"
+         "UNKNOWN" : "OUT",
+         "VACANT BUI" : "OUT"
+         "Vacant Dwe" : "OUT"
+         "VACANT LOT" : "OUT"
+         "Vehicle" : "OUT",
+         "WAREHOUSE" : "OUT",
+         "WHOLESALE/" : "IN",
+         "YARD" : "OUT",
+         "YARD/BUSIN" : "OUT",
+         "" : "STREET",
         }
 
 extractedDf["IN/OUT"] = extractedDf["Premise"].map(inOut)
